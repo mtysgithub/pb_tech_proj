31988d43031d480cd1cb5e0f8db185af6570ca5c
Ma Tinyau <0xmty.007@gmail.com>
2017-01-18 16:32:40 +0800

    commit

--- Assets/Script/UiMgr.cs
+++ Assets/Script/UiMgr.cs
@@ -1,15 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UiMgr : MonoBehaviour {
+public class UiMgr : MonoBehaviour
+{
+    public SceneMgr SceneMgr;
+    public GameObject MainView;
+    public GameObject PauseView;
+    public GameObject EndView;
+    public GameObject GamingView;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Awake()
+    {
+        MainView.SetActive(true);
+        PauseView.SetActive(false);
+        EndView.SetActive(false);
+        GamingView.SetActive(false);
+    }
+
+    public void BeginClick()
+    {
+        Debug.Log("BeginClick");
+
+        if (SceneMgr.ActiveScene == null)
+        {
+            GameObject tPlayerA = PlayerCtor.Instance.CreatLocalPlayer();
+            Player playerA = tPlayerA.GetComponent<Player>();
+            playerA.id = 1;
+
+            SceneCtr tScene = SceneMgr.Make(0.05f);
+            tScene.InitializePlayer(playerA);
+
+            tScene.Begin();
+
+            MainView.SetActive(false);
+            GamingView.SetActive(true);
+        }
+    }
+
+    public void LoadClick()
+    {
+        Debug.Log("LoadClick");
+    }
+
+    public void QuitClick()
+    {
+        Debug.Log("ExistClick");
+    }
 }
